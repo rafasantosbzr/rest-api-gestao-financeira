b4630079a84556dc8fd3e029d1fc7d0b467bb5a1
rafasantosbzr <rafasantosbzr@icloud.com>
2023-10-08 15:42:59 -0300

    implementação de endpoint para realização de login

--- src/controladores/usuarios.js
+++ src/controladores/usuarios.js
@@ -1,5 +1,7 @@
 const pool = require('../conexao');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const senhaJwt = require('../senhaJwt');
 
 const cadastrarUsuario = async (req, res) => {
 const { nome, email, senha } = req.body;
@@ -26,13 +28,41 @@ try {
 
     return res.status(201).json(usuario);
 } catch (error) {
-    console.log(error.message);
     return res.status(500).json({ mensagem: 'Erro interno do servidor' });
 }
 };
 
 const fazerLogin = async (req, res) => {
+const { email, senha } = req.body;
 
+if (!email || !senha) {
+    return res.status(400).json({ mensagem: 'Todos os campos são obrigatórios!' });
+}
+
+try {
+    const validarUsuario = `SELECT * FROM usuarios WHERE email = $1`;
+    const usuarioValidado = await pool.query(validarUsuario, [email]);
+
+    if (usuarioValidado.rowCount < 1) {
+        return res.status(400).json({ mensagem: 'Usuário e/ou senha inválido(s).' });
+    };
+
+    const senhaValidada = await bcrypt.compare(senha, usuarioValidado.rows[0].senha);
+
+    if (!senhaValidada) {
+        return res.status(400).json({ mensagem: 'Usuário e/ou senha inválido(s).' });
+    };
+
+    const token = jwt.sign({ id: usuarioValidado.rows[0].id }, senhaJwt, {
+        expiresIn: 28800
+    });
+
+    const { senha: _, ...usuario } = usuarioValidado.rows[0];
+
+    return res.status(200).json({usuario, token});
+} catch (error) {
+    return res.status(500).json({ mensagem: 'Erro interno do servidor' });
+}
 };
 
 const detalharUsuario = async (req, res) => {
