0034a4dbe19dcc51fd0c794b15e03604f76879db
rafasantosbzr <rafasantosbzr@icloud.com>
2023-10-08 13:00:23 -0300

    criacao das rotas

--- src/rotas.js
+++ src/rotas.js
@@ -1,7 +1,24 @@
 const express = require('express');
+const { cadastrarUsuario, fazerLogin, detalharUsuario, editarUsuario } = require('./controladores/usuarios');
+const { listarCategorias } = require('./controladores/categorias');
+const { listarTransacoes, detalharTransacao, cadastrarTransacao, editarTransacao, removerTransacao, obterExtrato } = require('./controladores/transacoes');
 
 const rotas = express();
 
-rotas.post('/usuario');
+rotas.post('/usuario', cadastrarUsuario);
+rotas.post('/login', fazerLogin);
+rotas.get('/usuario', detalharUsuario);
+rotas.put('/usuario', editarUsuario);
+
+rotas.get('/categoria', listarCategorias);
+
+rotas.get('/transacao', listarTransacoes);
+rotas.get('/transacao/:id', detalharTransacao);
+rotas.post('/transacao', cadastrarTransacao);
+rotas.put('/transacao/:id', editarTransacao);
+rotas.delete('/transacao/:id', removerTransacao);
+rotas.get('/transacao/extrato', obterExtrato);
+
 
 module.exports = rotas;
+
