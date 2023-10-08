631c853088bb975188e0e4826230b8bb59f9bb8b
rafasantosbzr <rafasantosbzr@icloud.com>
2023-10-08 17:28:00 -0300

    implementação de endpoint para listagem de categorias

--- src/controladores/categorias.js
+++ src/controladores/categorias.js
@@ -1,5 +1,14 @@
-const listarCategorias = async (req, res) => {
+const pool = require('../conexao');
 
+const listarCategorias = async (req, res) => {
+try {
+    const buscarCategorias = `SELECT * FROM categorias`;
+    const { rows } = await pool.query(buscarCategorias);
+
+    return res.status(200).json(rows);
+} catch (error) {
+    res.status(500).json({ mensagem: 'Erro interno do servidor' });
+}
 };
 
 module.exports = {
