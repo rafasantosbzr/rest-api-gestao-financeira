48834ed1b9684d1321235cbe9f6b27aed8181543
rafasantosbzr <rafasantosbzr@icloud.com>
2023-10-08 12:16:51 -0300

    criacao dos endpoints

--- src/controladores/transacoes.js
+++ src/controladores/transacoes.js
@@ -2,6 +2,32 @@ const listarTransacoes = async (req, res) => {
 
 };
 
+const detalharTransacao = async (req, res) => {
+    
+};
+
+const cadastrarTransacao = async (req, res) => {
+
+};
+
+const editarTransacao = async (req, res) => {
+ 
+};
+
+const removerTransacao = async (req, res) => {
+
+};
+
+const obterExtrato = async (req, res) => {
+
+};
+
+
 module.exports = {
-    listarTransacoes
+    listarTransacoes,
+    detalharTransacao,
+    cadastrarTransacao,
+    editarTransacao,
+    removerTransacao,
+    obterExtrato
 };
---
+++ src/controladores/usuarios.js
@@ -0,0 +1,22 @@
+const cadastrarUsuario = async (req, res) => {
+
+};
+
+const fazerLogin = async (req, res) => {
+
+};
+
+const detalharUsuario = async (req, res) => {
+
+};
+
+const editarUsuario = async (req, res) => {
+
+};
+
+module.exports = {
+    cadastrarUsuario,
+    fazerLogin,
+    detalharUsuario,
+    editarUsuario
+};
